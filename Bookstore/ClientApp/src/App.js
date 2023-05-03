a289331d993bbbbedbf23f9605feffa2a580da36
ML <ml6503@gmail.com>
2023-05-03 13:52:12 +0000

    feat: add appcontroller to app

--- Bookstore/ClientApp/src/App.js
+++ Bookstore/ClientApp/src/App.js
@@ -7,14 +7,17 @@ import './custom.css';
 
 export default class App extends Component {
   static displayName = App.name;
-
+  constructor (appController ) {
+    this.appController = appController;
+  }
   render() {
     return (
       <Layout>
         <Routes>
           {AppRoutes.map((route, index) => {
             const { element, requireAuth, ...rest } = route;
-            return <Route key={index} {...rest} element={requireAuth ? <AuthorizeRoute {...rest} element={element} /> : element} />;
+            return <Route key={index} {...rest} element={requireAuth ? 
+            <AuthorizeRoute {...rest} element={element} appController={this.appController}/> : element} appController={this.appController} />;
           })}
         </Routes>
       </Layout>
--- Bookstore/ClientApp/src/index.js
+++ Bookstore/ClientApp/src/index.js
@@ -1,19 +1,23 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+firimport "bootstrap/dist/css/bootstrap.css";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
+import { AppController } from "../src/controllers/AppController";
 
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
-const rootElement = document.getElementById('root');
+const baseUrl = document.getElementsByTagName("base")[0].getAttribute("href");
+const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
+
+const appController = new AppController();
 
 root.render(
   <BrowserRouter basename={baseUrl}>
-    <App />
-  </BrowserRouter>);
+    <App appController={appController} />
+  </BrowserRouter>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
