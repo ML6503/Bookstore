552f485c0e5aabe76fbf31a4cc593df3233406da
ML <ml6503@gmail.com>
2023-05-03 11:26:53 +0000

    feat: add modelapibook

---
+++ Bookstore/ClientApp/src/models/BookApiModel.js
@@ -0,0 +1,37 @@
+import { ApiConfig } from "../api/ApiConfig";
+
+export class BookApiModel extends ApiConfig {
+  async getAllBooks() {
+    //const resp = await fetch("api/books");
+    const resp = await this.host.get("api/books");
+    const books = await resp.json().catch((err) => console.error("Error", err));
+
+    return books;
+  }
+
+  async getOneBook(id) {
+    const resp = await this.host.get(`api/books/${id}`);
+    const book = await resp.json().catch((err) => console.error("Error", err));
+
+    return book;
+  }
+
+  async addBook(id) {
+    const resp = await this.host.post(`api/books/${id}`, newBook);
+    const book = await resp.json().catch((err) => console.error("Error", err));
+    await this.getAllBooks();
+    return book;
+  }
+
+  async updateBook(id, updatedBook) {
+    const resp = await this.host.put(`api/books/${id}`, updatedBook);
+    await resp.json().catch((err) => console.error("Error", err));
+    await this.getAllBooks();
+  }
+
+  async deleteBook(id) {
+    const resp = await this.host.put(`api/books/${id}`);
+    await resp.json().catch((err) => console.error("Error", err));
+    await this.getAllBooks();
+  }
+}
